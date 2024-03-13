69fea0f1d39fbec985b5dbb5470d176f6962126c
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2024-03-12 22:26:26 -1200

    api

---
+++ backends/index.js
@@ -0,0 +1,25 @@
+import express  from 'express' 
+import  body_parser from 'body-parser'
+import rutaValidacion from './src/routes/autotenticaion.route.js'
+
+const servidor = express()
+
+servidor.use(body_parser.json())
+servidor.use(body_parser.urlencoded({extended: false}))
+
+
+servidor.set('view engine', 'ejs');
+servidor.set('views','./views');
+
+servidor.get('/document',(req,res)=>{
+    res.render('documentdevjrl.ejs');
+})
+
+servidor.use(express.static('./public'));
+
+servidor.use(rutaValidacion)
+
+
+servidor.listen(3000, () =>{
+    console.log("esta funcionando")
+})
---
+++ backends/src/controllers/autenticacion.js
@@ -0,0 +1,46 @@
+import { pool } from "../database/conexion.js";
+import  Jwt  from "jsonwebtoken";
+
+export const validar = async (req, res) => {
+
+    try {
+        let {nombre,password} = req.body;
+        let sql = `SELECT * from usuarios where nombre='${nombre}' and password='${password}'`;
+
+        const [rows] = await pool.query(sql)
+        if (rows.length>0) {
+            let token=Jwt.sign({rows},process.env.AUT_SECRET,{expiresIn:process.env.AUT_EXPIRE})
+            return res.status(200).json({'nombre':rows, 'token':token,message:'token generado con exito'})
+        }else{
+            return res.status(404).json({"message":"Usuario no autorizado"})
+        }
+
+    } catch (error) {
+        res.status(500).json({status: 500, message: 'Error del servidor' + error})
+    }
+    
+}
+
+export const validarToken = async (req, res, next) => {
+
+    try {
+        
+        let tokenClient = req.headers['token']
+
+        if(!tokenClient){
+            return res.status(403).json({'message': 'Token es requerido'})
+        }else{
+            const token = Jwt.verify(tokenClient, process.env.AUT_SECRET, (error, decoded) => {
+                if(error){
+                    return res.status(403).json({message: 'Token es obligatorio'})
+                }else{
+                    next()
+                }
+            })
+        }
+
+    } catch (error) {
+        return res.status(500).json({status: 500, message: 'Error del servidor' + error})
+    }
+    
+}

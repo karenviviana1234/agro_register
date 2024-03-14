61fdf0ae520a633ca5c5c88c76dbb545474f3a91
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2024-03-13 20:49:22 -1200

    cambios nuevos

--- backends/index.js
+++ backends/index.js
@@ -1,14 +1,6 @@
 import express  from 'express' 
 import  body_parser from 'body-parser'
-<<<<<<< HEAD
-import rutaUsuario from './src/routes/UsuariosRoutesdevkvd.js'
 import rutaValidacion from './src/routes/autotenticaion.route.js'
-
-=======
-import rutaValidacion from './src/routes/autotenticaion.route.js'
-import router from '../backends/src/routes/Finca.routes.js'
-import rutaUsuario from './src/routes/routes.usuarios.js'
->>>>>>> 65c9722001aeb3d32b6068801593234d8b275612
 const servidor = express()
 
 servidor.use(body_parser.json())
@@ -25,8 +17,7 @@ servidor.get('/document',(req,res)=>{
 servidor.use(express.static('./public'));
 
 servidor.use(rutaValidacion)
-servidor.use(rutaUsuario)
-servidor.use(router)
+
 
 
 servidor.listen(3000, () =>{

59cc2ad98af6d3b3b77878ddf6e3ebc89d8d6d35
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2024-03-13 12:34:26 -1200

    cambios actividad

--- backends/index.js
+++ backends/index.js
@@ -1,7 +1,6 @@
 import express  from 'express' 
 import  body_parser from 'body-parser'
 import rutaValidacion from './src/routes/autotenticaion.route.js';
-import { rutaDeActividad } from './src/routes/Actividad.route.js';
 const servidor = express()
 
 servidor.use(body_parser.json())
@@ -19,7 +18,6 @@ servidor.get('/document',(req,res)=>{
 servidor.use(express.static('./public'));
 
 servidor.use(rutaValidacion)
-servidor.use(rutaDeActividad)
 
 
 

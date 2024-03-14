5dd3f6f3477114454efe00653bcea23e87cfb19a
joseronaldo1 <ledezmaparrajoseronaldo@gmail.com>
2024-03-14 09:44:17 -0500

    verificacion de autenticacion

--- backends/src/routes/InversionesroutesdevJrl.js
+++ backends/src/routes/InversionesroutesdevJrl.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { Validateinversiones, actualizar} from "../../validate/InversioesdevJrl.js";
 import { BuscarInversiones, actualizarInversiones, listarInversiones, registrarInversiones} from "../controllers/InvesionescontrollerdevJrl.js";
+import { validarToken } from "../controllers/autenticacion.js";
 
 const inversiones = Router()
 
-inversiones.get('/listarInversion',listarInversiones)
-inversiones.post('/RegistrarInversion',Validateinversiones, registrarInversiones)
-inversiones.get('/BuscarInversion/:id_Inversiones',BuscarInversiones)
-inversiones.put('/ActualizarInversion/:id_Inversiones',actualizar, actualizarInversiones)
+
+inversiones.get('/listarInversion',validarToken,listarInversiones)
+inversiones.post('/RegistrarInversion',validarToken,Validateinversiones, registrarInversiones)
+inversiones.get('/BuscarInversion/:id_Inversiones',validarToken,BuscarInversiones)
+inversiones.put('/ActualizarInversion/:id_Inversiones',validarToken,actualizar, actualizarInversiones)
 
 
 export default inversiones

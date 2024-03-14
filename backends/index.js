3370105243b7269c91f4c8223866823345518eb0
DANIKOOO <118011869+bravodaniel12@users.noreply.github.com>
2024-03-14 12:36:15 -0500

    Revert "Devdfg"

--- backends/index.js
+++ backends/index.js
@@ -1,6 +1,8 @@
 import express  from 'express' 
 import  body_parser from 'body-parser'
-import rutaValidacion from './src/routes/autotenticaion.route.js';
+import rutaValidacion from './src/routes/autotenticaion.route.js'
+import router from '../backends/src/routes/Finca.routes.js'
+import rutaUsuario from './src/routes/routes.usuarios.js'
 const servidor = express()
 
 servidor.use(body_parser.json())
@@ -10,15 +12,15 @@ servidor.use(body_parser.urlencoded({extended: false}))
 servidor.set('view engine', 'ejs');
 servidor.set('views','./views');
 
-
 servidor.get('/document',(req,res)=>{
-    res.render('document_devdfg.ejs');
+    res.render('document.ejs');
 })
 
 servidor.use(express.static('./public'));
 
 servidor.use(rutaValidacion)
-
+servidor.use(rutaUsuario)
+servidor.use(router)
 
 
 servidor.listen(3000, () =>{

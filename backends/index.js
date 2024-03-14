152443bb7e6fce4fb2a158df54103bc9753156c8
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2024-03-13 20:24:53 -1200

    Revert "Merge pull request #19 from karenviviana1234/devsdv"
    This reverts commit 2bf8fbc4c63496378d3b0b7e945afa3caf2c58af, reversing
    changes made to 3c5ed81e4f6c6516fddb1cd54841a4862f2d911b.

--- backends/index.js
+++ backends/index.js
@@ -1,7 +1,8 @@
 import express  from 'express' 
 import  body_parser from 'body-parser'
 import rutaValidacion from './src/routes/autotenticaion.route.js'
-
+import router from '../backends/src/routes/Finca.routes.js'
+import rutaUsuario from './src/routes/routes.usuarios.js'
 const servidor = express()
 
 servidor.use(body_parser.json())
@@ -12,12 +13,14 @@ servidor.set('view engine', 'ejs');
 servidor.set('views','./views');
 
 servidor.get('/document',(req,res)=>{
-    res.render('documentCultivosdevSdva.ejs');
+    res.render('document.ejs');
 })
 
 servidor.use(express.static('./public'));
 
 servidor.use(rutaValidacion)
+servidor.use(rutaUsuario)
+servidor.use(router)
 
 
 servidor.listen(3000, () =>{

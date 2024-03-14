61374a94d1f852c2a2edd2da9d303be9ab469a58
bravodaniel12 <danigobra2020@gmail.com>
2024-03-14 00:52:50 -0500

    otro modificacion

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express  from 'express' 
 import  body_parser from 'body-parser'
 import rutaValidacion from './src/routes/autotenticaion.route.js'
+import rutaUsuario from './src/routes/UsuariosRoutesdevkvd.js'
+import rutaValidacion from './src/routes/autotenticaion.routedevkvd.js'
 
 const servidor = express()
 
@@ -8,7 +10,18 @@ servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extended: false}))
 
 
+servidor.set('view engine', 'ejs');
+servidor.set('views','./views');
+
+servidor.use(rutaUsuario)
+servidor.get('/document',(req,res)=>{
+    res.render('documentdevjrl.ejs');
+})
+
+servidor.use(express.static('./public'));
+
 servidor.use(rutaValidacion)
+
 
 servidor.listen(3000, () =>{
     console.log("esta funcionando")

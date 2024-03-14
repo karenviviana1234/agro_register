ce6d6ff272ae5f588a858d8ca2c8d10e8c70442e
bravodaniel12 <danigobra2020@gmail.com>
2024-03-14 00:07:29 -0500

    super cambios

--- index.js
+++ index.js
@@ -8,17 +8,7 @@ servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extended: false}))
 
 
-servidor.set('view engine', 'ejs');
-servidor.set('views','./views');
-
-servidor.get('/document',(req,res)=>{
-    res.render('documentdevjrl.ejs');
-})
-
-servidor.use(express.static('./public'));
-
 servidor.use(rutaValidacion)
-
 
 servidor.listen(3000, () =>{
     console.log("esta funcionando")

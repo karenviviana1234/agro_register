2cada5749ccf528ee83c8219e5be637a1fcbdd3c
Joseronaldo1 <ledezmaparrajoseronaldo@gmail.com>
2024-03-17 09:12:45 -1200

    base de datos

--- backends/src/database/agro_register (1).sql
+++ backends/src/database/agro_register (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-03-2024 a las 21:50:12
+-- Tiempo de generación: 17-03-2024 a las 22:07:43
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
